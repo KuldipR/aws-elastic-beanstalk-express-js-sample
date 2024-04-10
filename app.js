3d3bfb0af826e11836e5212475370ea61d1c6a54
Kuldip <ranavayakuldip@gmail.com>
2024-04-09 21:56:45 -0700

    First Change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! I am Practising AWS EC2'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
